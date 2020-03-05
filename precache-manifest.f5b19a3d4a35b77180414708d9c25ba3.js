20e0415ab004e2f660f88b43bd4c8c917a702d0b
Dalalsa <sarebdesign@gmail.com>
2020-03-05 08:32:30 +0300

    Updates

---
+++ precache-manifest.f5b19a3d4a35b77180414708d9c25ba3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "170917c00609a9e507fb871b5eaa1fd7",
+    "url": "/restaurants-suggestions/index.html"
+  },
+  {
+    "revision": "878459715ac0639c219b",
+    "url": "/restaurants-suggestions/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "657c4d9f5f64e956fd0f",
+    "url": "/restaurants-suggestions/static/css/main.d0d9f14e.chunk.css"
+  },
+  {
+    "revision": "878459715ac0639c219b",
+    "url": "/restaurants-suggestions/static/js/2.94471469.chunk.js"
+  },
+  {
+    "revision": "657c4d9f5f64e956fd0f",
+    "url": "/restaurants-suggestions/static/js/main.b9bba38e.chunk.js"
+  },
+  {
+    "revision": "a58a9d6e7c523b677a59",
+    "url": "/restaurants-suggestions/static/js/runtime-main.7892688a.js"
+  }
+]);
